86bf568ef5535659f5057100c00ae8dbaf41318e
udsardor <udsardor@gmail.com>
2022-12-05 14:54:17 +0500

    header and footer have finished

---
+++ src/layout/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <nav>
+      <div class="nav-wrapper header__nav">
+        <a href="#" class="brand-logo">
+        Search from here
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="/">Movie Application</a>
+          </li>
+          <li>
+            <a href="https://t.me/SardiDev">Programmer</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
---
+++ src/layout/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main (){
+    return (
+        <div className="container content">
+            ALL CONTENT
+        </div>
+    )
+}
