639fa52dc3ec06ae61abeeffdf274d34468f2e15
udsardor <udsardor@gmail.com>
2022-12-06 10:48:42 +0500

    Types of films has finished

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,82 @@
 import { Component } from "react";
+import { LoaderSpiner } from "./Loader";
 
-export default class Search extends Component{
-    state ={
-        search: ""
+export default class Search extends Component {
+  state = {
+    search: "ip man",
+    type: "all",
+  };
+
+  handleKey = (e) => {
+    if (e.key === "Enter") {
+      this.props.searchMovie(this.state.search, this.state.type);
     }
-    handleKey = (e) => {
-        if(e.key === 'Enter'){
-            this.props.searchMovie(this.state.search)
-        }
-    }
-    render(){
-        return(
-            <div className="row">
-            <div className="col s12">
-              <div className="row">
-                <div className="input-field col s12">
-                  <input 
-                    type="search"
-                    id="autocomplete-input" 
-                    placeholder="Search..." 
-                    className="autocomplete" 
-                    value={this.state.search} 
-                    onChange={(e => this.setState({search: e.target.value}))}
-                    onKeyDown={this.handleKey} 
+  };
+  handleFilter = (e) => {
+    this.setState(() => ({type: e.target.dataset.type}), () => {
+      this.props.searchMovie(this.state.search, this.state.type ? this.state.type : <LoaderSpiner />)
+    });
+  };
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                type="search"
+                id="autocomplete-input"
+                placeholder="Search..."
+                className="autocomplete"
+                value={this.state.search}
+                onChange={(e) => this.setState({ search: e.target.value })}
+                onKeyDown={this.handleKey}
+              />
+              <button
+                className="btn search-btn"
+                onClick={() => this.props.searchMovie(this.state.search, this.state.type ? this.state.type : <LoaderSpiner />)}
+              >
+                Search
+              </button>
+              <div>
+                <label>
+                  <input
+                    className="with-gap"
+                    name="type"
+                    type="radio"
+                    data-type="all"
+                    onChange={this.handleFilter}
+                    checked={this.state.type === "all"}
                   />
-                  <button className="btn search-btn" onClick={() => this.props.searchMovie(this.state.search)}>
-                    Search
-                  </button>
-                </div>
+                  <span>All</span>
+                </label>
+                <label>
+                  <input
+                    className="with-gap"
+                    name="type"
+                    type="radio"
+                    data-type="movie"
+                    onChange={this.handleFilter}
+                    checked={this.state.type === "movie"}
+                  />
+                  <span>Movies only</span>
+                </label>
+                <label>
+                  <input
+                    className="with-gap"
+                    name="type"
+                    type="radio"
+                    data-type="series"
+                    onChange={this.handleFilter}
+                    checked={this.state.type === "series"}
+                  />
+                  <span>Series only</span>
+                </label>
               </div>
             </div>
           </div>
-        )
-    }
+        </div>
+      </div>
+    );
+  }
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -6,23 +6,23 @@ import Search from "../components/Search";
 export default class Main extends Component {
   state = {
     movies: [],
+
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=5744e32&s=Panda`)
+    fetch(`http://www.omdbapi.com/?apikey=5744e32&s=ip man`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search }));
   }
 
-  searchMovies = (str) => {
-      fetch(`http://www.omdbapi.com/?apikey=5744e32&s=${str}`)
+  searchMovies = (str, type) => {
+      fetch(`http://www.omdbapi.com/?apikey=5744e32&s=${str}${type !== "all" ? `&type=${type}`: ""}`)
         .then((response) => response.json())
         .then((data) => this.setState({ movies: data.Search }));
   }
 
   render(){
     return (
-
         <div className="container content">
           <Search searchMovie={this.searchMovies}/>
             {this.state.movies.length ? (
